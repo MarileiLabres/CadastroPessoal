2c6fca50cac925d490129d0d1fafa2d2163d281f
Marilei da Silva Labres Gimenes <mariisilva.96.ms@gmail.com>
2021-10-08 19:51:22 -0300

    Alteração Label

--- CadastroPessoal/Form1.Designer.cs
+++ CadastroPessoal/Form1.Designer.cs
@@ -33,21 +33,21 @@ namespace CadastroPessoal
             this.campoSobrenome = new System.Windows.Forms.TextBox();
             this.campoEndereco = new System.Windows.Forms.TextBox();
             this.campoData = new System.Windows.Forms.DateTimePicker();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.campoCidade = new System.Windows.Forms.TextBox();
             this.campoEmail = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
             this.campoTelefone = new System.Windows.Forms.TextBox();
             this.campoCep = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.campoMasculino = new System.Windows.Forms.CheckBox();
             this.campoFeminino = new System.Windows.Forms.CheckBox();
             this.campoOutro = new System.Windows.Forms.CheckBox();
             this.btCadastro = new System.Windows.Forms.Button();
+            this.lCadastro = new System.Windows.Forms.Label();
+            this.labelDadosPessoais = new System.Windows.Forms.Label();
+            this.labelDataNasc = new System.Windows.Forms.Label();
+            this.labelContato = new System.Windows.Forms.Label();
+            this.labelOrient = new System.Windows.Forms.Label();
+            this.labelEndereco = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // campoNome
@@ -86,50 +86,6 @@ namespace CadastroPessoal
             this.campoData.TabIndex = 3;
             this.campoData.Value = new System.DateTime(2021, 10, 8, 17, 59, 11, 0);
             // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(364, 146);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(118, 18);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "Data de nascimento:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(347, 84);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(131, 22);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.Text = "Dados Pessoais:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.Location = new System.Drawing.Point(347, 331);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 6;
-            this.textBox3.Text = "Contato:";
-            // 
-            // textBox4
-            // 
-            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox4.Location = new System.Drawing.Point(347, 253);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 7;
-            this.textBox4.Text = "Endereço:";
-            // 
             // campoCidade
             // 
             this.campoCidade.Location = new System.Drawing.Point(197, 294);
@@ -145,17 +101,6 @@ namespace CadastroPessoal
             this.campoEmail.PlaceholderText = "Digite seu e-mail...";
             this.campoEmail.Size = new System.Drawing.Size(131, 23);
             this.campoEmail.TabIndex = 9;
-            // 
-            // textBox7
-            // 
-            this.textBox7.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox7.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox7.Location = new System.Drawing.Point(364, 12);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 27);
-            this.textBox7.TabIndex = 10;
-            this.textBox7.Text = "Cadastro";
             // 
             // campoTelefone
             // 
@@ -210,18 +155,6 @@ namespace CadastroPessoal
             this.comboBox1.TabIndex = 13;
             this.comboBox1.Text = "UF";
             // 
-            // textBox5
-            // 
-            this.textBox5.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox5.Location = new System.Drawing.Point(29, 195);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.PlaceholderText = "Digite o CEP...";
-            this.textBox5.Size = new System.Drawing.Size(128, 18);
-            this.textBox5.TabIndex = 14;
-            this.textBox5.Text = "Orientação Sexual:";
-            // 
             // campoMasculino
             // 
             this.campoMasculino.AutoSize = true;
@@ -263,26 +196,87 @@ namespace CadastroPessoal
             this.btCadastro.Text = "Cadastrar";
             this.btCadastro.UseVisualStyleBackColor = false;
             // 
+            // lCadastro
+            // 
+            this.lCadastro.AutoSize = true;
+            this.lCadastro.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lCadastro.Location = new System.Drawing.Point(364, 33);
+            this.lCadastro.Name = "lCadastro";
+            this.lCadastro.Size = new System.Drawing.Size(123, 37);
+            this.lCadastro.TabIndex = 19;
+            this.lCadastro.Text = "Cadastro";
+            this.lCadastro.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelDadosPessoais
+            // 
+            this.labelDadosPessoais.AutoSize = true;
+            this.labelDadosPessoais.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelDadosPessoais.Location = new System.Drawing.Point(347, 88);
+            this.labelDadosPessoais.Name = "labelDadosPessoais";
+            this.labelDadosPessoais.Size = new System.Drawing.Size(148, 28);
+            this.labelDadosPessoais.TabIndex = 20;
+            this.labelDadosPessoais.Text = "Dados Pessoais:";
+            // 
+            // labelDataNasc
+            // 
+            this.labelDataNasc.AutoSize = true;
+            this.labelDataNasc.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelDataNasc.Location = new System.Drawing.Point(375, 142);
+            this.labelDataNasc.Name = "labelDataNasc";
+            this.labelDataNasc.Size = new System.Drawing.Size(134, 19);
+            this.labelDataNasc.TabIndex = 21;
+            this.labelDataNasc.Text = "Data de nascimento:";
+            // 
+            // labelContato
+            // 
+            this.labelContato.AutoSize = true;
+            this.labelContato.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelContato.Location = new System.Drawing.Point(357, 331);
+            this.labelContato.Name = "labelContato";
+            this.labelContato.Size = new System.Drawing.Size(87, 28);
+            this.labelContato.TabIndex = 22;
+            this.labelContato.Text = "Contato:";
+            // 
+            // labelOrient
+            // 
+            this.labelOrient.AutoSize = true;
+            this.labelOrient.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelOrient.Location = new System.Drawing.Point(29, 193);
+            this.labelOrient.Name = "labelOrient";
+            this.labelOrient.Size = new System.Drawing.Size(121, 19);
+            this.labelOrient.TabIndex = 23;
+            this.labelOrient.Text = "Orientação Sexual:";
+            // 
+            // labelEndereco
+            // 
+            this.labelEndereco.AutoSize = true;
+            this.labelEndereco.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelEndereco.Location = new System.Drawing.Point(347, 248);
+            this.labelEndereco.Name = "labelEndereco";
+            this.labelEndereco.Size = new System.Drawing.Size(97, 28);
+            this.labelEndereco.TabIndex = 24;
+            this.labelEndereco.Text = "Endereço:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(856, 539);
+            this.Controls.Add(this.labelEndereco);
+            this.Controls.Add(this.labelOrient);
+            this.Controls.Add(this.labelContato);
+            this.Controls.Add(this.labelDataNasc);
+            this.Controls.Add(this.labelDadosPessoais);
+            this.Controls.Add(this.lCadastro);
             this.Controls.Add(this.btCadastro);
             this.Controls.Add(this.campoOutro);
             this.Controls.Add(this.campoFeminino);
             this.Controls.Add(this.campoMasculino);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.campoCep);
             this.Controls.Add(this.campoTelefone);
-            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.campoEmail);
             this.Controls.Add(this.campoCidade);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.campoData);
             this.Controls.Add(this.campoEndereco);
             this.Controls.Add(this.campoSobrenome);
@@ -300,21 +294,21 @@ namespace CadastroPessoal
         private System.Windows.Forms.TextBox campoSobrenome;
         private System.Windows.Forms.TextBox campoEndereco;
         protected System.Windows.Forms.DateTimePicker campoData;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox campoCidade;
         private System.Windows.Forms.TextBox campoEmail;
-        private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox campoTelefone;
         private System.Windows.Forms.TextBox campoCep;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.CheckBox campoMasculino;
         private System.Windows.Forms.CheckBox campoFeminino;
         private System.Windows.Forms.CheckBox campoOutro;
         private System.Windows.Forms.Button btCadastro;
+        private System.Windows.Forms.Label lCadastro;
+        private System.Windows.Forms.Label labelDadosPessoais;
+        private System.Windows.Forms.Label labelDataNasc;
+        private System.Windows.Forms.Label labelContato;
+        private System.Windows.Forms.Label labelOrient;
+        private System.Windows.Forms.Label labelEndereco;
     }
 }
 
